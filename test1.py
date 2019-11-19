3740c4f6f6bf91c9185485ecd740644d3729fd0f
Baraadaraghmeh <51743582+Baraadaraghmeh@users.noreply.github.com>
2019-11-19 16:05:50 +0200

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,103 @@
+#q1
+print('hello', 'hello','   hello'  ,sep='\n    ')
+print('\n')
+#q2............................................................
+print ('orange academy\n'*20)
+print('\n')
+#q3............................................................
+x=float(1)
+y=float(2.8)
+z=float("3")
+w=float("4.2")
+print(x,y,z,w,sep='10\n')
+print('\n')
+#q4.............................................................
+n1= int (input('enter your first number:'))
+n2=int (input('enter your second number:'))
+result=(n1*n2)
+print('result=',result)
+#q5.............................................................
+n1= int (input('enter  number1:'))
+n2= int (input('enter  number2:'))
+n3= int (input('enter  number3:'))
+n4= int (input('enter  number4:'))
+n5= int (input('enter  number5:'))
+sum=(n1+n2+n3+n4+n5)
+avg=(sum/5)
+print('avg=',avg)
+print('\n')
+#q6..............................................................
+x=1
+y=2.8
+z=1j
+o="Orange"
+A=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
+print(type(A))
+#q7..............................................................
+x,y='orange',1
+X1,y1=100,-10
+print(x)
+print(y)
+print(X1)
+print(y1)
+#q8...............................................................
+x=10
+print(x)
+X_q='orange'
+print('X_q')
+AB=14
+print(AB)
+print(10*10)
+#q9.................................................................
+#print(hello world)
+print('Cheers', 'Mate')#this is the program
+"""Test test
+
+"""
+#q10................................................................
+name=str(input('Your name:'))
+age=int(input('Enter your Age:'))
+result=100-age
+print(result+2019)
+#q11.................................................................
+base=int(input('Enter base of rectangle:'))
+height=int(input('Enter height:'))
+area=0.5*base*height
+print('area=',area)
+#q12................................................................
+x=11
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(abs(x*-1))
+print(int(x))
+print(float(x))
+print(divmod(x,y))
+print(pow(x,y))
+print(x**y)
+print(x>y)
+print(x==y)
+print(x!=y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
